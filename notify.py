e380714ae99ce0eb183496d9669fd01434ded3dd
Palakjain01 <60611126+Palakjain01@users.noreply.github.com>
2020-05-26 23:47:13 +0530

    acc commit

--- notify.py
+++ notify.py
@@ -6,7 +6,7 @@ smtp_server ="smtp.gmail.com"
 sender_email="iampalakjain01@gmail.com"    #Sender's Mail Address
 receiver_email="itspalak19@gmail.com"      #Receiver's Mail Address
 password="xecbeupbulzfwpos"
-if accuracy > 90:
+if accuracy > 80:
     message="""    Subject: Report | Prediction Program
     
     CONGRATULATIONS! 
@@ -21,5 +21,3 @@ context=ssl.create_default_context()
 with smtplib.SMTP_SSL(smtp_server, port, context=context) as server:
     server.login(sender_email,password)
     server.sendmail(sender_email, receiver_email, message)
-    
-    
